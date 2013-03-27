e8ae6edcbbdad7bcec37ddd47baa7fbfbb056f59
Sagar Rohankar <sagar.rohankar@gmail.com>
2013-03-27 22:56:41 +0530

    first commit

---
+++ game-of-life/src/main/java/com/rohankar/App.java
@@ -0,0 +1,13 @@
+package com.rohankar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
