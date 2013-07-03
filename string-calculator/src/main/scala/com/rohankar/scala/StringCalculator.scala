b7ad83406d1f3417047b9d321c0f99153af2262b
Sagar Rohankar <sagar.rohankar@gmail.com>
2013-07-03 22:15:15 +0530

    completed string calculator kata

--- string-calculator/src/main/scala/com/rohankar/scala/StringCalculator.scala
+++ string-calculator/src/main/scala/com/rohankar/scala/StringCalculator.scala
@@ -2,15 +2,36 @@ package com.rohankar.scala
 
 object StringCalculator {
 
-  def add(value: String): Int = {
-    if (value == "")
-      0
-    else {
-      val numbers: Array[Int] = value.split(",").map(_.trim.toInt)
-      val negNumbers: Array[Int] = numbers.filter(_ < 0)
-      if (!negNumbers.isEmpty)
-        throw new IllegalArgumentException("negatives not allowed - " + negNumbers.mkString)
-      numbers.sum
+  def add(string: String): Int = {
+    if (string == null || string.isEmpty) return 0
+
+    var tempString = string
+    var delimiter = ","
+
+    if (string startsWith "//") {
+      delimiter = string.charAt(2).toString
+      tempString = string.substring(3, string.length)
     }
+
+    if (tempString contains ("\n")) {
+      val nos = tempString.split("\n")
+      (for (s <- nos) yield {
+        if (s contains (delimiter)) {
+          calculateSumForDelimiterSeparatedString(s, delimiter)
+        } else {
+          s.trim.toInt
+        }
+      }).sum
+    } else {
+      calculateSumForDelimiterSeparatedString(tempString, delimiter)
+    }
+  }
+
+  private def calculateSumForDelimiterSeparatedString(string: String, delimiter: String = ","): Int = {
+    val numbers: Array[Int] = string.split(delimiter).map(_.trim.toInt)
+    val negNumbers: Array[Int] = numbers.filter(_ < 0)
+    if (!negNumbers.isEmpty)
+      throw new IllegalArgumentException("negatives not allowed - " + negNumbers.mkString)
+    numbers.sum
   }
 }
--- string-calculator/src/test/scala/com/rohankar/scala/StringCalculatorTest.scala
+++ string-calculator/src/test/scala/com/rohankar/scala/StringCalculatorTest.scala
@@ -6,35 +6,31 @@ import org.scalatest.matchers.ShouldMatchers
 class StringCalculatorTest extends FunSuite with ShouldMatchers {
 
   test("add calculate 0 for empty string") {
-    // when
-    val sum = StringCalculator.add("")
-
-    // then
-    sum should be(0)
+    StringCalculator.add("") should be(0)
   }
 
   test("add calculate number itself when single number passed") {
-    // when
-    val sum = StringCalculator.add("1");
-
-    // then
-    sum should be(1)
+    StringCalculator.add("1") should be(1)
   }
 
   test("add calculcate sum when two numbers passed") {
-    // when
-    val sum = StringCalculator.add("1, 2")
-
-    // then
-    sum should be(3)
+    StringCalculator.add("1, 2") should be(3)
   }
 
   test("negative numbers not allowed") {
-    // when
     val exception = intercept[IllegalArgumentException] {
       StringCalculator.add("-1")
     }
 
     exception.getMessage should be("negatives not allowed - -1")
   }
+
+  test("new line character allowed for separating numbers") {
+    StringCalculator.add("1\n2,3") should be(6)
+  }
+
+  test("custom delimiter") {
+    StringCalculator.add("//;1;2") should be(3)
+  }
+
 }
